0ceb3cb16f7f05e2119e5b43c2c60d5c6125cf91
Fareed-TI <fkandalaft@tradeinformatics.com>
2021-08-19 13:44:31 -0400

    synced on TI laptop. Moved Header Component within <BrowserRouter>

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,9 @@ import StreamShow from './streams/StreamShow';
 const App = () => {
   return (
     <div className='ui container'>
-      <Header />
       <BrowserRouter>
-        <div>
+      <div>
+          <Header />
           <Route path='/' exact component={StreamList} />
           <Route path='/streams/new' component={StreamCreate} />
           <Route path='/streams/delete' component={StreamDelete} />
