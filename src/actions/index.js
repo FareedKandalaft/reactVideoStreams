750e966ee7c397db93067d0e37ad0063418fce1f
FareedKandalaft <fareed@spectrecoder.com>
2021-09-28 21:37:18 -0400

    {createBrowserHistory} from history as per react-redux-dom >= 4.0

--- src/actions/index.js
+++ src/actions/index.js
@@ -26,6 +26,8 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post('/streams', { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
+
+  // on success send user back to the <root />
 };
 export const fetchStreams = () => async (dispatch) => {
   const response = await streams.get('/streams');
