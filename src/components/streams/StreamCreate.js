c9025004b6c4902c0e4aa8f523f333f5f505d816
FareedKandalaft <fareed@spectrecoder.com>
2021-09-29 21:52:15 -0400

    isolation of React-Router call in StreamEdit to fetchStream on mount. Refactor StreamForm and implement code reuse of StreamCreate

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,66 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
 import { createStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamCreate extends React.Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className='ui error message'>
-          <div className='header'>{error}</div>
-        </div>
-      );
-    }
-  }
-
-  renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete='off' />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
-
   onSubmit = (formVals) => {
     this.props.createStream(formVals);
   };
 
   render() {
     return (
-      <form
-        className='ui form error'
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-      >
-        <Field name='title' component={this.renderInput} label='Enter Title' />
-        <Field
-          name='description'
-          component={this.renderInput}
-          label='Enter Description'
-        />
-        <button className='ui button primary'>Submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = (formValues) => {
-  const errs = {};
-  if (!formValues.title) {
-    errs.title = 'Title can not be blank';
-  }
-  if (!formValues.description) {
-    errs.description = 'Description can not be blank';
-  }
-  return errs;
-};
-
-const formWrapped = reduxForm({
-  form: 'streamCreate',
-  validate,
-})(StreamCreate);
-
-export default connect(null, { createStream })(formWrapped);
+export default connect(null, { createStream })(StreamCreate);
---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class StreamForm extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className='ui error message'>
+          <div className='header'>{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete='off' />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit = (formVals) => {
+    this.props.onSubmit(formVals);
+  };
+
+  render() {
+    return (
+      <form
+        className='ui form error'
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field name='title' component={this.renderInput} label='Enter Title' />
+        <Field
+          name='description'
+          component={this.renderInput}
+          label='Enter Description'
+        />
+        <button className='ui button primary'>Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errs = {};
+  if (!formValues.title) {
+    errs.title = 'Title can not be blank';
+  }
+  if (!formValues.description) {
+    errs.description = 'Description can not be blank';
+  }
+  return errs;
+};
+
+export default reduxForm({
+  form: 'streamForm',
+  validate,
+})(StreamForm);
